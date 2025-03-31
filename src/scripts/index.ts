4f624b94d256aa309f3d0b9c9f1edc060affd082
krushna rathod <krushna.rathod@simformsolutions.com>
2025-03-31 12:13:48 +0530

    feature: initalize Calculaotr in index.ts

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -1,1 +1,11 @@
-console.log("hello");
+import { Calculator } from "./calculator";
+
+new Calculator(
+  ".calculator",
+  ".input-field-text",
+  ".memory-functions button, .deg-functions button, .trigonometry-functions button, .numeric-operator-buttons button",
+  "button[value='f-e']",
+  "button[value='sin']",
+  "button[value='cos']",
+  "button[value='tan']"
+);
